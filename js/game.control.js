88057ca17812bd67573cf47f4d325a6050a0e8b7
marieme <ps.marieme95@gmail.com>
2020-04-19 18:19:23 +0200

    Terrain + Score + Raquettes + Balle en mouvement

--- js/game.control.js
+++ js/game.control.js
@@ -1,10 +1,15 @@
 // Contrôle du jeu
 
 game.control = {
-    onKeyDown : function(event)
-    {
+    onKeyDown : function(event) {
+        if ( event.keyCode === game.keycode.KEYDOWN ) {
+        } else if ( event.keyCode === game.keycode.KEYUP ) {
+        }
     },
-    onKeyUp : function(event)
-    {
+
+    onKeyUp : function(event) {
+        if ( event.keyCode === game.keycode.KEYDOWN ) {
+        } else if ( event.keyCode === game.keycode.KEYUP ) {
+        }
     }
 };
