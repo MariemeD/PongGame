643677eb72c2d281a7bad94be7eb490952cdfe61
marieme <ps.marieme95@gmail.com>
2020-04-27 23:03:41 +0200

    V2 IA + Perte de balle + Score + Multi + Nettoyage code et modification :  - Serveur + commentaires  - CSS  - IA + Commentaires

--- game/js/game.ai.js
+++ game/js/game.ai.js
@@ -1,68 +1,56 @@
-game.ai = {
-    player : null,
-    ball : null,
-    setPlayerAndBall : function(player, ball) {
+class ai {
+    constructor(player, ball) {
         this.player = player;
         this.ball = ball;
-    },
-
-    move : function() {
-        if ( this.ball.directionX === 1 ) {
-            if ( this.player.originalPosition === "right" ) {
-                // Suivre
-                this.followBall();
-            }
-            if ( this.player.originalPosition === "left" ) {
-                // centrer
-                this.goCenter();
-            }
-        } else {
-            if ( this.player.originalPosition === "right" ) {
-                // centrer
-                this.goCenter();
-            }
-            if ( this.player.originalPosition === "left" ) {
-                // Suivre
-                this.followBall();
-            }
-        }
-    },
-
-    // Suivre
-    followBall : function() {
-        if ( this.ball.posY < this.player.posY + this.player.height/2 ) {
-            // la position de la balle est sur l'écran, au dessus de celle de la raquette
-            this.player.posY --;
-        } else if ( this.ball.posY > this.player.posY + this.player.height/2 ) {
-            // la position de la balle est sur l'écran, en dessous de celle de la raquette
-            this.player.posY ++;
-        }
-    },
-
-    // Centrer
-    goCenter : function() {
-        if ( this.player.posY + this.player.height/2 > game.groundHeight / 2 ) {
-            this.player.posY --;
-        } else if ( this.player.posY + this.player.height/2 < game.groundHeight / 2 ) {
-            this.player.posY ++;
-        }
-    },
-
-    startBall : function() {
-        if ( this.player.originalPosition === "right" ) {
-            this.ball.inGame = true;
-            this.ball.posX = this.player.posX;
-            this.ball.posY = this.player.posY;
-            this.ball.directionX = -1;
-            this.ball.directionY = 1;
-        } else {
-            this.ball.inGame = true;
-            this.ball.posX = this.player.posX + this.player.width;
-            this.ball.posY = this.player.posY;
-            this.ball.directionX = 1;
-            this.ball.directionY = 1;
-        }
-
+        this.enabled = true;
     }
 
-};
+    // Mouvement : alterne suivre et centrer
+    move() {
+        if(this.enabled === true)
+        {
+            if ( this.ball.directionX === 1 ) {
+                if ( this.player.originalPosition === "right" ) {
+                    // suivre
+                    return this.followBall();
+                }
+                if ( this.player.originalPosition === "left" ) {
+                    // centrer
+                    return this.goCenter();
+                }
+            } else {
+                if ( this.player.originalPosition === "right" ) {
+                    // centrer
+                    return this.goCenter();
+                }
+                if ( this.player.originalPosition === "left" ) {
+                    // suivre
+                    return this.followBall();
+                }
+            }
+        }
+    }
+
+    // Calque des mouvements de la raqueete sur la balle.
+    // En gros la raquette suit la balle quand c'est l'IA
+    followBall() {
+        if ( this.ball.posY < this.player.posY + this.player.height/2 ) {
+            // la position de la balle est sur l'écran, au dessus de celle de la raquette
+            this.player.posY -= 3;
+        } else if ( this.ball.posY > this.player.posY + this.player.height/2 ) {
+            // la position de la balle est sur l'écran, en dessous de celle de la raquette
+            this.player.posY += 3;
+        }
+        return this.player.posY;
+    }
+
+    // La raquette se repositionne au centre
+    goCenter() {
+        if ( this.player.posY + this.player.height/2 > game.groundHeight / 2 ) {
+            this.player.posY--;
+        } else if ( this.player.posY + this.player.height/2 < game.groundHeight / 2 ) {
+            this.player.posY++;
+        }
+        return this.player.posY;
+    }
+}
